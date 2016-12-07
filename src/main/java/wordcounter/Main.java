c92f108aecc67b47ef131e0d5ee1d3b95cba07cf
phamm <pwilli@tsn.at>
2016-12-07 16:24:43 +0100

    ignores now .classpath & .project

--- src/main/java/wordcounter/Main.java
+++ src/main/java/wordcounter/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[]args)
 	{
-		Counter c = new Counter("C:\\Users\\phamm\\Desktop\\count.txt");
-		c.analyse();
+		Wordcounter c = new Wordcounter("C:\\Users\\phamm\\Desktop\\count.txt");
+		c.startcounting();
 	}
 }
